28f2633bd4dc7766fd661adeb9fe8b2757ebcb36
trobbierob <troberts@my.hpu.edu>
2016-10-27 23:11:38 -0400

    Added ChangeListener to Question 1 RadioButtons

--- app/src/main/java/com/example/android/quizii/UchihaActivity.java
+++ app/src/main/java/com/example/android/quizii/UchihaActivity.java
@@ -1,53 +1,118 @@
 package com.example.android.quizii;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import static com.example.android.quizii.R.id.q1_radioGroup;
 
 public class UchihaActivity extends AppCompatActivity {
 
-    private int score = 0;
+    int score = 0;
+
+    boolean q3_a = false;
+    boolean q3_b = false;
+    boolean q3_c = false;
+    boolean q3_d = false;
+
+    //private RadioGroup q1_radioGroup;
+    //private RadioGroup q2_radioGroup;
+    private RadioGroup q1radioGroup;
+    private RadioGroup q2radioGroup;
+
+    private static final String TAG = "Tag";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uchiha);
+
+        /* Initialize Radio Group and attach click handler */
+        q1radioGroup = (RadioGroup) findViewById(q1_radioGroup);
+        q1radioGroup.clearCheck();
+
+        /* Attach CheckedChangeListener to radio group */
+        q1radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if(checkedId == 2131427424){
+                    Log.v(TAG, "checkedID is true" + checkedId);
+                    Toast.makeText(UchihaActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                    score++;
+                } else {
+
+                    Toast.makeText(UchihaActivity.this, "salad", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+
+        });
+
+        /* Initialize Radio Group and attach click handler */
+        q2radioGroup = (RadioGroup) findViewById(R.id.q2_radioGroup);
+        q2radioGroup.clearCheck();
+
+        /* Attach CheckedChangeListener to radio group */
+        q2radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                if(null!=rb && checkedId > -1){
+                    Toast.makeText(UchihaActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+
     }
-/*
+
+    public void onClear(View view) {
+
+        q1radioGroup.clearCheck();
+        q2radioGroup.clearCheck();
+
+    }
+
     public void Question1(View view) {
         boolean checked = ((RadioButton) view).isChecked();
 
         switch (view.getId()){
-            case R.id.u_q_radio1a:
+            case R.id.q1_true:
                 if (checked)
-                    score++;
+                    return;
                 break;
-            case R.id.u_q_radio1b:
+            case R.id.q1_false:
                 if (checked)
-                    score = score + 0;
+                    score = score + 1;
                 break;
 
         }
 
     }
-*/
+
     public void Question2() {
 
 
 
     }
-/*
+
     public void Question3(View view) {
 
         boolean checked = ((CheckBox) view).isChecked();
 
-        boolean q3_a = false;
-        boolean q3_b = false;
-        boolean q3_c = false;
-        boolean q3_d = false;
-
         // Check which checkbox was clicked
         switch(view.getId()) {
-            case R.id.checkbox_q3_a:
+            case R.id.q3_a:
                 if (checked) {
                     q3_a = true;
                 } else {
@@ -57,7 +122,7 @@ public class UchihaActivity extends AppCompatActivity {
 
         // Check which checkbox was clicked
         switch(view.getId()) {
-            case R.id.checkbox_q3_b:
+            case R.id.q3_b:
                 if (checked) {
                     q3_b = true;
                 } else {
@@ -67,7 +132,7 @@ public class UchihaActivity extends AppCompatActivity {
 
         // Check which checkbox was clicked
         switch(view.getId()) {
-            case R.id.checkbox_q3_c:
+            case R.id.q3_c:
                 if (checked) {
                     q3_c = true;
                 } else {
@@ -77,7 +142,7 @@ public class UchihaActivity extends AppCompatActivity {
 
         // Check which checkbox was clicked
         switch(view.getId()) {
-            case R.id.checkbox_q3_d:
+            case R.id.q3_d:
                 if (checked) {
                     q3_d = true;
                 } else {
@@ -85,14 +150,14 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        if (q3_a == true && q3_b == true && q3_d == true) {
+        // If a, b, and c
+        if (q3_a == true && q3_b == true && q3_c == true) {
             score++;
         } else {
             return;
-        } break;
+        }
 
     }
-*/
 
     public void Question4() {
 
@@ -100,7 +165,31 @@ public class UchihaActivity extends AppCompatActivity {
 
     }
 
+    public void Submit(View view) {
+        Context context = getApplicationContext();
 
+        if (score == 4) {
 
+            Toast.makeText(context, "100% " + score, Toast.LENGTH_SHORT).show();
+
+        } else if (score == 3) {
+
+            Toast.makeText(context, "75% " + score, Toast.LENGTH_SHORT).show();
+
+        } else if (score == 2) {
+
+            Toast.makeText(context, "50% " + score, Toast.LENGTH_SHORT).show();
+
+        } else if (score == 1) {
+
+            Toast.makeText(context, "25% " + score, Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Toast.makeText(context, "0% " + score, Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
 
 }
