7b415b986838fc8f59f2a40b911467368238d46d
trobbierob <troberts@my.hpu.edu>
2016-10-28 20:37:41 -0400

    Added logic and questions for SenjuActivity. Question 4 has not been started on the logic side.

--- app/src/main/java/com/example/android/quizii/SenjuActivity.java
+++ app/src/main/java/com/example/android/quizii/SenjuActivity.java
@@ -1,13 +1,174 @@
 package com.example.android.quizii;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SenjuActivity extends AppCompatActivity {
+
+    int q1 = 0;
+    int q2 = 0;
+    int q3 = 0;
+    int q4 = 0;
+    int score = 0;
+
+    Spinner spinner;
+
+    RadioGroup q2radioGroup;
+    RadioGroup q3radioGroup;
+    RadioButton q3_radio_tobirama;
+    RadioButton q3_radio_hiruzen;
+    RadioButton q3_radio_jiraiya;
+    RadioButton q3_radio_nagato;
+
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_senju);
+
+        spinner = (Spinner) findViewById(R.id.senju_spinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.kekkei_genkai,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String itemSelected = parent.getItemAtPosition(position).toString();
+
+                if (itemSelected.equals("Wood Release")) {
+                    Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position)
+                            + " is selected", Toast.LENGTH_SHORT).show();
+                    q1 = 1;
+
+                } else {
+
+                    q1 = 0;
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        /* Radio Group for the second Question */
+        q2radioGroup = (RadioGroup) findViewById(R.id.q2_radioGroup);
+
+        /* Attach CheckedChangeListener to radio group */
+        q2radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.q2_itama) {
+                    q2 = 1;
+                } else {
+                    q2 = 0;
+                }
+
+            }
+
+        });
+
+        q3_radio_tobirama = (RadioButton) findViewById(R.id.q3_tobirama);
+        q3_radio_hiruzen = (RadioButton) findViewById(R.id.q3_hiruzen);
+        q3_radio_jiraiya = (RadioButton) findViewById(R.id.q3_jiraiya);
+        q3_radio_nagato = (RadioButton) findViewById(R.id.q3_nagato);
+
+        q3_radio_tobirama.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                q3_radio_tobirama.setChecked(true);
+                q3_radio_hiruzen.setChecked(false);
+                q3_radio_jiraiya.setChecked(false);
+                q3_radio_nagato.setChecked(false);
+
+                q3 = 1;
+            }
+        });
+
+        q3_radio_hiruzen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                q3_radio_tobirama.setChecked(false);
+                q3_radio_hiruzen.setChecked(true);
+                q3_radio_jiraiya.setChecked(false);
+                q3_radio_nagato.setChecked(false);
+
+                q3 = 0;
+            }
+        });
+
+        q3_radio_jiraiya.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                q3_radio_tobirama.setChecked(false);
+                q3_radio_hiruzen.setChecked(false);
+                q3_radio_jiraiya.setChecked(true);
+                q3_radio_nagato.setChecked(false);
+
+                q3 = 0;
+            }
+        });
+
+        q3_radio_nagato.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                q3_radio_tobirama.setChecked(false);
+                q3_radio_hiruzen.setChecked(false);
+                q3_radio_jiraiya.setChecked(false);
+                q3_radio_nagato.setChecked(true);
+
+                q3 = 0;
+            }
+        });
+
     }
+
+    public void Question4(View view) {
+
+
+
+
+
+    }
+
+    public void Submit(View view) {
+        Context context = getApplicationContext();
+
+        score = q1 + q2 + q3 + q4;
+
+        if (score == 4) {
+
+            Toast.makeText(context, "100%!", Toast.LENGTH_SHORT).show();
+
+        } else if (score == 3) {
+
+            Toast.makeText(context, "75%", Toast.LENGTH_SHORT).show();
+
+        } else if (score == 2) {
+
+            Toast.makeText(context, "50%", Toast.LENGTH_SHORT).show();
+
+        } else if (score == 1) {
+
+            Toast.makeText(context, "25%", Toast.LENGTH_SHORT).show();
+
+        } else {
+
+            Toast.makeText(context, "0%", Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/android/quizii/UchihaActivity.java
+++ app/src/main/java/com/example/android/quizii/UchihaActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -13,11 +12,11 @@ import static com.example.android.quizii.R.id.q1_radioGroup;
 
 public class UchihaActivity extends AppCompatActivity {
 
-    int score = 0;
     int q1 = 0;
     int q2 = 0;
     int q3 = 0;
     int q4 = 0;
+    int score = 0;
 
     boolean q3_a = false;
     boolean q3_b = false;
@@ -29,53 +28,41 @@ public class UchihaActivity extends AppCompatActivity {
     boolean q4_c = false;
     boolean q4_d = false;
 
-    //private RadioGroup q1_radioGroup;
-    //private RadioGroup q2_radioGroup;
     RadioGroup q1radioGroup;
     RadioGroup q2radioGroup;
-
-    private static final String TAG = "Tag";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uchiha);
 
-        /* Initialize Radio Group and attach click handler */
+        /* Radio Group for the first Question */
         q1radioGroup = (RadioGroup) findViewById(q1_radioGroup);
-        q1radioGroup.clearCheck();
 
         /* Attach CheckedChangeListener to radio group */
         q1radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+            public void onCheckedChanged(RadioGroup group, int checkedId) {;
                 if(checkedId == R.id.q1_true){
                     q1 = 0;
                 } else {
-                    q1++;
+                    q1 = 1;
                 }
 
             }
 
         });
 
-        /* Initialize Radio Group and attach click handler */
+        /* Radio Group for the second Question */
         q2radioGroup = (RadioGroup) findViewById(R.id.q2_radioGroup);
-        q2radioGroup.clearCheck();
 
         /* Attach CheckedChangeListener to radio group */
         q2radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton rb = (RadioButton) group.findViewById(checkedId);
                 if(checkedId == R.id.q2_itachi){
-                    //Log.v(TAG, "checkedID is false " + checkedId);
-                    //Toast.makeText(UchihaActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
-                    q2++;
+                    q2 = 1;
                 } else {
-                    //Toast.makeText(UchihaActivity.this, rb.getText(), Toast.LENGTH_SHORT).show();
                     q2 = 0;
                 }
 
@@ -85,35 +72,11 @@ public class UchihaActivity extends AppCompatActivity {
 
     }
 
-    public void onClear(View view) {
-
-        q1radioGroup.clearCheck();
-        q2radioGroup.clearCheck();
-
-    }
-
-    public void Question1(View view) {
-        boolean checked = ((RadioButton) view).isChecked();
-
-        switch (view.getId()){
-            case R.id.q1_true:
-                if (checked)
-                    return;
-                break;
-            case R.id.q1_false:
-                if (checked)
-                    score = score + 1;
-                break;
-
-        }
-
-    }
-
     public void Question3(View view) {
 
         boolean checked = ((CheckBox) view).isChecked();
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Tobi is selected
         switch(view.getId()) {
             case R.id.q3_a:
                 if (checked) {
@@ -123,7 +86,7 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Madara is selected
         switch(view.getId()) {
             case R.id.q3_b:
                 if (checked) {
@@ -133,7 +96,7 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Obito is selected
         switch(view.getId()) {
             case R.id.q3_c:
                 if (checked) {
@@ -143,7 +106,7 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // Check which checkbox was clicked
+        // ChecK CheckBox when Pain is selected
         switch(view.getId()) {
             case R.id.q3_d:
                 if (checked) {
@@ -153,9 +116,11 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // If a, b, and c
-        if (q3_a == true && q3_b == true && q3_c == true) {
-            q3++;
+        /* Obito Uchiha went by several names. Although he wasn't the
+        * real Madara, he became a symbol for it.
+        * http://naruto.wikia.com/wiki/Obito_Uchiha */
+        if (q3_a && q3_b && q3_c && !q3_d) {
+            q3 = 1;
         } else {
             q3 = 0;
         }
@@ -166,7 +131,7 @@ public class UchihaActivity extends AppCompatActivity {
 
         boolean checked = ((CheckBox) view).isChecked();
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Sasuke is selected
         switch(view.getId()) {
             case R.id.q4_a:
                 if (checked) {
@@ -176,7 +141,7 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Fugaku is selected
         switch(view.getId()) {
             case R.id.q4_b:
                 if (checked) {
@@ -186,7 +151,7 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Indra is selected
         switch(view.getId()) {
             case R.id.q4_c:
                 if (checked) {
@@ -196,7 +161,7 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // Check which checkbox was clicked
+        // Check CheckBox when Kagami is selected
         switch(view.getId()) {
             case R.id.q4_d:
                 if (checked) {
@@ -206,9 +171,13 @@ public class UchihaActivity extends AppCompatActivity {
                 } break;
         }
 
-        // If a, b, and c
-        if (q4_c == true && !q4_a && !q4_b && !q4_d) {
-            q4++;
+        /* This one is sort of a trick question. Here's a note from wikia:
+         * During his debut, Indra's Mangekyō was depicted with a swirl
+         * pattern. Later in the anime, Indra's Mangekyō Sharingan pattern
+         * was changed to be the same as Sasuke's.
+         * http://naruto.wikia.com/wiki/Indra_Ōtsutsuki */
+        if (q4_a && q4_c && !q4_b && !q4_d) {
+            q4 = 1;
         } else {
             q4 = 0;
         }
@@ -222,23 +191,23 @@ public class UchihaActivity extends AppCompatActivity {
 
         if (score == 4) {
 
-            Toast.makeText(context, "100% " + score, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "100%!", Toast.LENGTH_SHORT).show();
 
         } else if (score == 3) {
 
-            Toast.makeText(context, "75% " + score, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "75%", Toast.LENGTH_SHORT).show();
 
         } else if (score == 2) {
 
-            Toast.makeText(context, "50% " + score, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "50%", Toast.LENGTH_SHORT).show();
 
         } else if (score == 1) {
 
-            Toast.makeText(context, "25% " + score, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "25%", Toast.LENGTH_SHORT).show();
 
         } else {
 
-            Toast.makeText(context, "0% " + score, Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "0%", Toast.LENGTH_SHORT).show();
 
         }
 
